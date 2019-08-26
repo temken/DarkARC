26cde696e7ed59d0b2675d5f8e3fbbbbfdc4d4a6
Timon Emken <emken@chalmers.se>
2019-08-26 11:01:06 +0200

    Parallelized the code with MPI

--- src/main.py
+++ src/main.py
@@ -1,23 +1,51 @@
 import time
 
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0: print("MPI processes: ",size)
+
 from units import *
 from radial_integrals import *
 from tabulation import *
 
 start_tot = time.time()
 
-
-lPrime_max = 7
-
 integral = 1
 element = Ar
+lPrime_max = 7
+gridsize = 100
+
 n = 3
 l = 1
-lPrime = 7 
-L = 8
 
-create_integration_method_table(integral,element,n,l,lPrime,L,100)
-tabulate_integral(integral,element,n,l,lPrime,L,100)
+job_list = list()
+for lPrime in range(lPrime_max+1):
+	for L in range( min(abs(l-1-lPrime),abs(l+1-lPrime)) , l+1+lPrime+1):
+		filepath = "../data/integral_" + str(integral) + "/" + element.Shell_Name(n,l) + "_" + str(lPrime)+ "_" + str(L) + ".txt"
+		if os.path.exists(filepath) == False :
+			job_list.append([integral,element,n,l,lPrime,L])
+		# tabulate_integral(integral,element,n,l,lPrime,L,100,rank)
+my_jobs = job_list[divide_work(len(job_list),size,rank)[0]:divide_work(len(job_list),size,rank)[1]+1]
+
+for job in my_jobs:
+	tabulate_integral(job[0],job[1],job[2],job[3],job[4],job[5],gridsize,rank)
+
+print(len(job_list))
+
+lPrime = 7 
+L = 7
+
+# start1 = time.time()
+# create_integration_method_table(integral,element,n,l,lPrime,L,10)
+# end1 = time.time()
+# start2 = time.time()
+# tabulate_integral(integral,element,n,l,lPrime,L,100)
+# end2 = time.time()
+
+# print(end1-start1,end2-start2)
 
 # k = 0.2 * keV
 # q = 600 * keV
--- src/tabulation.py
+++ src/tabulation.py
@@ -9,28 +9,36 @@ qMax = 1000*keV
 kMin = 0.2*keV
 kMax = 100*keV
 
-def tabulate_integral(integral,element,n,l,lPrime,L,steps=100):
-    filepath = "../data/I_" + str(integral) + "_" + element.Shell_Name(n,l) + "_" + str(lPrime)+ "_" + str(L) + ".txt"
-    qGrid = np.logspace(np.log10(qMin),np.log10(qMax),steps)
-    kGrid = np.logspace(np.log10(kMin),np.log10(kMax),steps)
+def tabulate_integral(integral,element,n,l,lPrime,L,steps,rank):
+    filepath = "../data/integral_" + str(integral) + "/" + element.Shell_Name(n,l) + "_" + str(lPrime)+ "_" + str(L) + ".txt"
+    if os.path.exists(filepath) == False:
 
-    integration_methods = create_integration_method_table(integral,element,n,l,lPrime,L,steps)
-    table = list()
-    counter = 0;
+        print("Start tabulation - Rank: ",rank,"\tn=",n,"\tl=",l,"\tlPrime=",lPrime,"\tL=",L)
 
-    for k in kGrid:
-        q_list=list()
-        for q in qGrid:
-            integration_method = get_integration_method(integration_methods, k,q)
-            result = radial_integral(integral, element, n, l, k, lPrime, L, q,integration_method)
-            counter+=1
-            print("q = ", q/keV," keV\tk = ",k/keV," keV\tI = ",result,"\tMethod: ",integration_method,"\tProgress: ",100*counter/steps/steps)
-            q_list.append(result)
-        table.append(q_list)
-    np.savetxt(filepath,table,delimiter = '\t')
+        qGrid = np.logspace(np.log10(qMin),np.log10(qMax),steps)
+        kGrid = np.logspace(np.log10(kMin),np.log10(kMax),steps)
+
+        integration_methods = create_integration_method_table(integral,element,n,l,lPrime,L,steps)
+        table = list()
+        counter = 0;
+
+        for k in kGrid:
+            q_list=list()
+            for q in qGrid:
+                integration_method = get_integration_method(integration_methods, k,q)
+                result = radial_integral(integral, element, n, l, k, lPrime, L, q,integration_method)
+                counter+=1
+                if counter % 100 == 0:
+                    print("Rank: ",rank,"\tn=",n,"\tl=",l,"\tlPrime=",lPrime,"\tL=",L,"Progress: ",100*counter/steps/steps," %")
+                # print("q = ", q/keV," keV\tk = ",k/keV," keV\tI = ",result,"\tMethod: ",integration_method,"\tProgress: ",100*counter/steps/steps)
+                q_list.append(result)
+            table.append(q_list)
+        np.savetxt(filepath,table,delimiter = '\t')
+        print("Finish tabulation - Rank: ",rank,"\tn=",n,"\tl=",l,"\tlPrime=",lPrime,"\tL=",L)
+
 
 def create_integration_method_table(integral,element,n,l,lPrime,L,steps):
-    filepath = '../data/integration_methods_'+str(integral)+'_' + element.Shell_Name(n,l)+'_'+str(lPrime)+str(L)+'.txt'
+    filepath = '../data/methods_'+str(integral)+'/' + element.Shell_Name(n,l)+'_'+str(lPrime)+str(L)+'.txt'
     if os.path.exists(filepath):
         print("Method table exists already and will be imported.")
         methods = np.loadtxt(filepath,dtype = 'str')
@@ -38,7 +46,7 @@ def create_integration_method_table(integral,element,n,l,lPrime,L,steps):
 
     else:
         print("Method table must be created.")
-        coarse_gridsize = steps //10 +1
+        coarse_gridsize = steps // 10 +1
         print("Start evaluating the coarse grid of size ",coarse_gridsize,"x",coarse_gridsize," for ",element.Shell_Name(n,l)," with lPrime = ",lPrime," and L = ",L)
         qGridCoarse = np.logspace(np.log10(qMin),np.log10(qMax),coarse_gridsize)
         kGridCoarse = np.logspace(np.log10(kMin),np.log10(kMax),coarse_gridsize)
@@ -52,8 +60,7 @@ def create_integration_method_table(integral,element,n,l,lPrime,L,steps):
                 qi += 1
             ki += 1
         np.savetxt(filepath,methods,fmt="%s" ,delimiter = '\t')
-    
-    print("quadosc / Hankel / analytic [%] : ",sum( (m == 'quadosc').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100, " / ",sum( (m == 'Hankel').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100, " / ",sum( (m == 'analytic').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100)
+    # print("quadosc / Hankel / analytic [%] : ",sum( (m == 'quadosc').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100, " / ",sum( (m == 'Hankel').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100, " / ",sum( (m == 'analytic').sum() for m in methods)/coarse_gridsize/coarse_gridsize*100)
     return methods
 
 def identify_integration_method(integral,element,n,l,k,lPrime,L,q):
@@ -85,10 +92,23 @@ def get_integration_method(methods, k,q):
         if kGridCoarse[ki] > k :
             break;
     potential_methods = [methods[ki][qi],methods[ki-1][qi],methods[ki][qi-1],methods[ki-1][qi-1]]
-    # print(potential_methods)
     if "quadosc" in potential_methods:
         return "quadosc"
     elif "analytic" in potential_methods:
         return "analytic"
     else:
         return "Hankel"
+
+
+def divide_work(no_of_jobs,no_of_processes,rank):
+    count = no_of_jobs // no_of_processes;
+    remainder = no_of_jobs % no_of_processes;
+    start=0
+    stop=0
+    if rank < remainder:
+        start = rank*(count+1)
+        stop = start + count
+    else:
+        start = rank * count + remainder;
+        stop = start + (count - 1);
+    return [start,stop]
